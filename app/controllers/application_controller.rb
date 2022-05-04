6bf66b7b6d3525943fa63d786281a4e39b9bb00f
shivani2001-art <shivshakannan821@gmail.com>
2022-05-04 16:05:47 +0530

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "hello , world!"
-    end
-
-    def hellos
-        #render html:"!holla ,mundo!"
-    end
 end
