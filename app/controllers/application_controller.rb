ef00911eaaa2f543691d9722f99ac272436b077b
shivani2001-art <shivshakannan821@gmail.com>
2022-05-04 11:10:53 +0530

    Add a static pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def hello
+        render html: "hello , world!"
+    end
+
+    def hellos
+        #render html:"!holla ,mundo!"
+    end
 end
